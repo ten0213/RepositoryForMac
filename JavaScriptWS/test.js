0d63d22b8536cd9e1e9fd21cb6418cdf3796238b
ten0213 <ten02130735@gmail.com>
2022-09-06 23:36:36 +0900

    last

--- JavaScriptWS/test.js
+++ JavaScriptWS/test.js
@@ -1,18 +1,14 @@
 // program to find the factors of an integer
-console.log("hello world!");
+
 // take input
-const num = prompt('Enter a positive number: ');
 
-console.log(`The factors of ${num} is:`);
-
-function main() {
-    for(let i = 1; i <= num; i++) {
-
-    // check if number is a factor
-    if(num % i == 0) console.log(i);
-
-    }
-
-}
+console.log('The factors of 12 is:');
 
 // looping through 1 to num
+for(let i = 1; i <= 100; i++) {
+
+    // check if number is a factor
+    if(100 % i == 0) {
+        console.log(i);
+    }
+}
---
+++ pythonWS/test.py
@@ -0,0 +1,7 @@
+def add(a, b, c):
+    if a > 0:
+        return a + b + c
+
+if __name__ == "__main__":
+    for i in range(10):
+        print(add(10, 20, 30))
