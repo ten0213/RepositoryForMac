c80dffd7a40b9fffa436249ed0dcd02f23c1cb9a
ten0213 <ten02130735@gmail.com>
2023-06-27 21:34:51 +0900

    last

--- C++WS/test.cpp
+++ C++WS/test.cpp
@@ -1,18 +1,19 @@
 #include "test.h"
+#include <string>
 using namespace std;
-
-// The class
-class MyClass
-{
-public: // Access specifier
-  MyClass()
-  { // Constructor
-    cout << "Hello World~!\t 안녕~" << endl;
-  }
-};
 
 int main()
 {
-  MyClass myObj; // Create an object of MyClass (this will call the constructor)
-  return 0;
+  int t;
+
+  while(true) {
+    cin.setf(ios_base::hex, ios_base::basefield);
+    cin >> t;
+    cout << "입력 :" << t << endl;
+    if(cin.fail()) {
+      cout << "제대로 입력해주세요" << endl;
+      cin.clear();
+    }
+    if(t == 0) break;
+  }
 }
--- javaws/test.java
+++ javaws/test.java
@@ -1,11 +1,47 @@
 package javaws;
+import java.util.Scanner;
 
 public class test {
-	public static void main(String[] args) {
-		System.out.println("Hello world!" + sum(1, 22));
-	}
+    //main 메소드 실행
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); // Scanner 객체 생성하기
 
-	public static int sum(int a, int b) {
-		return a+b;
-	}
+        System.out.println("hw3_1: 황재현");
+        System.out.print("정수 개수 입력: ");
+
+        int arraySize = sc.nextInt();
+        int[] array = new int[arraySize];
+
+        System.out.print(arraySize + "개의 정수 입력: ");
+
+        //for 반복문을 이용하여 배열에 수 입력하기
+        for(int i=0;i<array.length;i++)
+        	array[i] = sc.nextInt();
+
+        //최대값을 배열에서 알아내기 위한 변수 arrayMax
+        int arrayMax = array[0];
+
+        //최대값 찾아내기
+        for(int j : array) {
+        	if(arrayMax < j)
+        		arrayMax = j;
+        }
+
+        //배열에서 최대값을 제외하고 출력하기
+        System.out.print("최대값 제외 = ");
+        for (int k=0;k<array.length;k++) {
+            if (array[k] != arrayMax)
+                System.out.print(array[k] + " ");
+        }
+        int counter = 0; // 최대값 개수를 저장할 변수
+
+        //최대값 개수를 세어서 변수에 저장하기
+        System.out.print("\n최대값 개수 = ");
+        for(int l=0;l<array.length;l++) {
+            if (array[l] == arrayMax)
+                counter++;
+        }
+        System.out.print(counter); // 최댓값 개수 출력하기
+        sc.close(); // Scanner 닫기
+    }
 }
